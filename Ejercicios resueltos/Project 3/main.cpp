dca58eb70ffcfc1e55a16b6d14dcb4dbfd4a347b
T4t1n <jaimiskytn@gmail.com>
2024-12-03 18:34:34 -0600

    Cuarto commit para atualizar project 3 3/12/2024

--- Ejercicios resueltos/Project 3/main.cpp
+++ Ejercicios resueltos/Project 3/main.cpp
@@ -16,20 +16,49 @@ int main()
          system("color 70");
          cout << "\n\n\n\tPrograma que convierte la temperatura en grados Celsius a grados Fahrenheit, Kelvin y Rankime!\n";
          cout << "\t----------------------------------------------------------------------------------------------------\n";
-         cout << "\n\n\tDigite temperatura en grados Celsius";
 
+         do {
+         cout << "\n\n\tDigite temperatura en grados Celsius, que no sea menor a -273 grados:\n\n\t";
+         cin >> TempC;
 
-
-
-
-
-
-         cout << "\n\tDesea realizar otra operacion S/ N? ";
-         cin >> seguir;
-         while (seguir != "S" && seguir != "s" && seguir != "N" && seguir != "n")
-         {
-             cout << "\n\tError: Digite S o N: "; cin >>  seguir;
+         if(TempC < -273) {
+            system("cls");
+            cout << "\n\n\tError: debes digitar el dato correcto, no menor a -273 grados!\n\n\t";
+            Sleep(1500);
+            system("cls");
          }
+    }while(TempC < -273);
+
+    // Calculos.
+
+    //Calculo de Celsius a grados Fahrenheit
+    TempF = 9/5 * TempC + 32;
+    //Calculo de Celsius a grados Kelvin
+    TempK = TempC + 273;
+    //Calculo de Celsius a grados Rankime
+    TempR = TempF + 460;
+
+    /* ---- Imprimir datos y resultados ---- */
+    cout << "\n\t Imprimir datos y resultados \n";
+    cout << "\t-----------------------------------------------\n";
+    cout << endl;
+    cout << "\n\tGrados Celsius digitado: \t" << TempC << endl << endl;
+    cout << "\n\tResultado de grados Farenheit: \t" << TempF << endl << endl;
+    cout << "\n\tResultado de grados Kelvin: \t" << TempK << endl << endl;
+    cout << "\n\tResultado de grados Rankime: \t" << TempR << endl << endl;
+
+
+
+
+
+
+
+     cout << "\n\tDesea realizar otra operacion S/ N? ";
+     cin >> seguir;
+     while (seguir != "S" && seguir != "s" && seguir != "N" && seguir != "n")
+     {
+      cout << "\n\tError: Digite S o N: "; cin >>  seguir;
+     }
 
     }while(seguir == "S" || seguir == "s");
 
