1da7e49c7e3c0006e83132cf9e12b990e630e3ca
T4t1n <jaimiskytn@gmail.com>
2024-12-06 18:32:31 -0600

    Commit con actualizacion project 4 avanzado 6/12/2024

--- Ejercicios resueltos/Project 4/main.cpp
+++ Ejercicios resueltos/Project 4/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <locale.h>
 #include <windows.h>
+#include <limits>
 
 using namespace std;
 
@@ -12,14 +13,33 @@ int main()
     do
     {
         system("cls");
-        system("color 85");
+        system("color 75");
         setlocale(LC_ALL, "");
         cout << endl;
         cout << "\n\n\tPrograma que calcula el volumen  de titulación de una base!\n";
         cout << "\n\n\t-----------------------------------------------------------\n";
-        cout << "\n\tDigite la concentración en eq-g/L y volumen de la base en L:";
+        do {
+        cout << "\n\tDigite la concentración en eq-g/L y volumen de la base en L:\t";
+        cin >> Concentracion; cout << "\t"; cin >> volumen;
+        if(cin.fail()) {
+            system("cls");
+            cout << "\a";
+            cout << "\n\tError: En ambos datos deben ser numeros!\n";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            Sleep(1500);
+            system("cls");
+
+        }else {
+                break;
+            }
+
+        }while (true);
+
+        do {
 
 
+        }while (true);
 
 
 
@@ -33,7 +53,6 @@ int main()
                 cout << "\n\tError: Digite S o N!\n\n";
                 Sleep(1500);
                 system("cls");
-
             }
 
         }while(seguir != "S" && seguir != "s" && seguir != "N" && seguir != "n");
