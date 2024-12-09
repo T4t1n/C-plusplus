8da195995dc6a79d5dc5576500b5d20ad528438b
T4t1n <jaimiskytn@gmail.com>
2024-12-08 23:46:12 -0600

    Commit con actualizacion project 5 avance 8/12/2024

---
+++ Ejercicios resueltos/Project 5/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <locale.h>
+#include <windows.h>
+#include <limits>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int coefa, coefb, coefc, raiz1, raiz2;
+    string seguir = "S";
+    do
+    {
+
+        cout << "\n\tPrograma que calcula las raíces de una ecuación cuadrática!";
+        cout << "\n\t-----------------------------------------------------------";
+        do{
+            cout << "\n\n\tDigite los coeficientes a, b y c de la ecuación cuadrática:\n";
+            cout << "\t", cin >> coefa, cout << "\t", cin >> coefb, cout << "\t", cin >> coefc;
+            if(cin.fail()){
+                cout << "\n\tError: Los datos debes ser numericos!\n";
+                cin.clear();
+                cin.ignore();
+            }else {
+                break;
+            }
+        }while(true);
+
+        //Calculos
+        raiz1 = (-coefb + sqrt(pow(coefb, 2) -4 * coefa * coefc)) / (2 * coefa);
+        raiz2 = (-coefb - sqrt(pow(coefb, 2) -4 * coefa * coefc)) / (2 * coefa);
+        cout << "\n" << raiz1;
+        cout << "\n" << raiz2;
+
+
+
+        do {
+            cout << "\n\t¿Desea realizar otro proceso? S / N\t";
+            cin >> seguir;
+            if (seguir != "S" && seguir != "s" && seguir != "N" && seguir != "n"){
+                cout << "\n\tError: Digite S o N!\n";
+            }else {
+                break;
+            }
+        }while(true);
+    }while(seguir == "S" || seguir == "s");
+}
