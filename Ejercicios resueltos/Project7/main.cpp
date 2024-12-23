33741ffbc0ff5906b2b609b1a01083b1afec5c52
T4t1n <jaimiskytn@gmail.com>
2024-12-22 22:52:25 -0600

    Commit con actualizacion project 7 Avance - 22/12/2024

--- Ejercicios resueltos/Project7/main.cpp
+++ Ejercicios resueltos/Project7/main.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <locale.h>
 
+
 using namespace std;
 
 int main()
@@ -12,15 +13,48 @@ int main()
     setlocale(LC_ALL, "");
     system("title, Project 7");
     string seguir = "S";
-    int angulo, velocidad;
+    float angulo, velocidad;
+    float seno, resultadoAlcance, pi=M_PI;
     do {
         cout << "\n\n\tPrograma que calcula el alcance y altura máxima de un proyectil!";
         cout << "\n\t------------------------------------------------------------------";
-        do{
-            cout << "\n\n\tDigigte el ángulo > 0 y < 90 y velocidad inicial del proyectil en m/s";
+
+
+            cout << "\n\n\tDigite el ángulo > 0 y < 90 y velocidad inicial del proyectil en m/s:\t";
             cin >> angulo >> velocidad;
 
-        }while(angulo > 0 || angulo < 90 && velocidad > 0 || velocidad < 90);
+            while(cin.fail()){
+                cin.clear();
+                cin.ignore();
+                cout << "\n\n\tError: Debes digitar valores numericos!";
+                Sleep(1000);
+                system("cls");
+                cout << "\n\n\tDigite el ángulo > 0 y < 90 y velocidad inicial del proyectil en m/s:\t";
+                cin >> angulo >> velocidad;
+
+            };
+
+            while(angulo < 0 || angulo > 90 || velocidad < 0 || velocidad > 90){
+                cin.clear();
+                cin.ignore();
+                cout << "\n\n\tError: Debes digitar valores correctos!";
+                Sleep(1000);
+                system("cls");
+                cout << "\n\n\tDigite el ángulo > 0 y < 90 y velocidad inicial del proyectil en m/s:\t";
+                cin >> angulo >> velocidad;
+            };
+
+            // ---------------- Fórmula ´para el alcance máximo ----------------
+
+            angulo = angulo * pi / 180;
+            seno = sin(2*angulo);
+
+            resultadoAlcance = (pow(velocidad, 2) * 1) / 9.8;
+
+            cout << resultadoAlcance;
+
+
+
 
 
 
