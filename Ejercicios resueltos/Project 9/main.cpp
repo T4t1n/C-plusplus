c39502473eb9a8adb9e9ab40f539882ddcce594b
T4t1n <jaimiskytn@gmail.com>
2025-01-09 18:32:26 -0600

    Commit con actualizacion project 9 Avance - 09/01/2025

--- Ejercicios resueltos/Project 9/main.cpp
+++ Ejercicios resueltos/Project 9/main.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <locale.h>
 #include <math.h>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -11,7 +12,7 @@ int main()
 
     string seguir = "";
     double masa_Cilindro, radio_Cilindro, longitud_Cilindro;
-    const double Densidad_Aluminio = 2.7;
+    const double Densidad_Aluminio = 2.7, pi = M_PI;
     do {
 
         cout << "\n\n\tPrograma que calcula la longitud de un cilindro de aluminio!";
@@ -21,15 +22,28 @@ int main()
         cout << "\n\t-----------------------------";
         cout << "\n\n\tDigite la masa del cilindro en gramos:\t";
         cin >> masa_Cilindro;
+        while (!(cin >> masa_Cilindro)) {
+            cout << "\n\n\tError: Digite la masa del cilindro en gramos:\t";
+            cin.clear();
+            cin.ignore(123, '\n');
+
+        }
+
         cout << "\n\n\tDigite el radio del cilindro en cm:\t";
         cin >> radio_Cilindro;
 
-        cout >> "\n\n\tCalculando longitud del cilindro...";
-        longitud_Cilindro = masa_Cilindro / Densidad_Aluminio * M_PI * sqrt(radio_Cilindro);
+        cout << "\n\n\tCalculando longitud del cilindro...";
+        longitud_Cilindro = masa_Cilindro / (Densidad_Aluminio * ( pi * pow(radio_Cilindro, 2)));
         Sleep(700);
 
         cout << "\n\n\t\tImprimir datos y resultadors";
-        cout << "\n\n\t-----------------------------";
+        cout << "\n\t-----------------------------------";
+        cout << "\n\n\tMasa del cilindro en gramos:\t" << masa_Cilindro;
+        cout << "\n\tRadio del cilindro en cm:\t" << radio_Cilindro;
+        cout << "\n\tDensidad del aluminio g/cm^3:\t" << Densidad_Aluminio;
+        cout << "\n\tLongitud del cilindro en cm:\t" << longitud_Cilindro;
+        cout << "\n\t-----------------------------------";
+
 
 
         cout << "\n\n\tDesea procesar otro cilindro? : S/s o N/n\t";
